212378e003b378e86af66c14d4e0aa55f20a59e6
Thisal Dilmith <93121062+thisal-d@users.noreply.github.com>
2026-03-11 01:22:52 +0530

    update

---
+++ c/struct.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct Car{
+    int wheels = 4;
+    int tires = 3;
+};
+
+
+int main(){
+
+
+    Car car;
+
+    car.wheels = 1;
+    car.tires = 3;
+
+    Car* car_p = &car;
+
+    printf("%d\n", car.wheels);
+}
